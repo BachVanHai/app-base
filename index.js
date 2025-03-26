a2d55391b0e335096c1a6fadb54d203cb2710776
BachVanHai <haibv@bmktech.com.vn>
2025-03-26 14:38:51 +0700

    remove duplicate key lang

--- index.js
+++ index.js
@@ -4962,8 +4962,6 @@ var messages_vi = {
 	"menu.personalContracts": "Hợp đồng cá nhân",
 	"menu.partnerContracts": "Hợp đồng đối tác",
 	"menu.allContracts": "Tất cả hợp đồng",
-	"menu.exportAvailableContracts": "Xuất báo cáo",
-  "menu.transactionManage": "Quản lý giao dịch",
 	"menu.account": "Tài khoản",
 	"menu.createAccount": "Tạo mới tài khoản",
 	"menu.accountManagement": "Quản lý tài khoản",
