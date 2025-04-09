68270b08dbf4dee33c0d6d1aabbe78a2630ddf34
BachVanHai <haibv@bmktech.com.vn>
2025-04-09 16:26:38 +0700

    fix

--- index.modern.js
+++ index.modern.js
@@ -7349,7 +7349,6 @@ const DatePicker = props => {
     value: props.value,
     onClose: () => props.onClose && props.onClose(),
     onChange: date => props.onChange && props.onChange(date),
-    onBlur: date => props.onBlur && props.onBlur(date),
   }), /*#__PURE__*/React.createElement(Label, null, props.label), props.errors && props.touched && props.isShowErrorMessage && getPropObject(props.errors, props.fieldName) && getPropObject(props.touched, props.fieldName) ? /*#__PURE__*/React.createElement("div", {
     className: "text-danger"
   }, getPropObject(props.errors, props.fieldName)) : null);
@@ -7396,13 +7395,7 @@ const BaseFormDatePicker = ({
       if (onChange) {
         onChange(date, form);
       }
-    },
-      onBlur: function onBlur(date) {
-          form.setFieldValue(fieldName, date[0]);
-          if (onChange) {
-              onChange(date, form);
-          }
-      }
+    }
   })));
 };
 
