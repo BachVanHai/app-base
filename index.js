c0d501d5ae055c5dd9917caafca3c88412a5e3ec
DESKTOP-IVN453T\Admin <chudanhtuan2034@gmail.com>
2025-05-08 10:40:17 +0700

    revert

--- index.js
+++ index.js
@@ -585,30 +585,37 @@ var setUpHttpClient = function setUpHttpClient(store, apiBaseUrl) {
 
     switch (e.response.status) {
       case 400:
-      case 500: {
-        let clientMessageId;
-        clientMessageId = e.response.config.headers.clientmessageid || e.response.config.headers.clientMessageId || e.response.config.headers['client-message-id'] || null;
-        if (clientMessageId) {
-          toastInfo(e.response.data.message || `Có lỗi trong quá trình xử lý. Vui lòng cung cấp mã tra cứu của bạn cho IT BMK để được hỗ trợ sớm nhất. Mã tra cứu: ${clientMessageId}`);
-        } else {
+        const clientMessageId = e.response.config.headers.clientmessageid || 
+                           e.response.config.headers.clientMessageId || 
+                           e.response.config.headers['client-message-id'];
+        if(clientMessageId){
+          toastInfo(`Có lỗi trong quá trình xử lý. Vui lòng cung cấp mã tra cứu của bạn cho IT BMK để được hỗ trợ sớm nhất. Mã tra cứu: ${clientMessageId}`);
+        }else{
           toastInfo(e.response.data.message || /*#__PURE__*/React.createElement(FormattedMessage, {
-            id: e.response.status === 400 ? "common.error.400" : "common.error.500"
+            id: "common.error.400"
           }));
         }
         break;
-      }
-      case 403: {
+      case 403:
         if (e.response.data.error === 'Forbidden') {
           return e.response;
         }
-
-        toastError(e.response.data.message);
+        toastInfo(e.response.data.message || /*#__PURE__*/React.createElement(FormattedMessage, {
+          id: "common.sessionExpired"
+        }));
         store.dispatch({
           type: LOGOUT_ACTION
         });
         history.push('/login');
         break;
-      }
+      case 500:
+        if(clientMessageId){
+          toastInfo(`Có lỗi trong quá trình xử lý. Vui lòng cung cấp mã tra cứu của bạn cho IT BMK để được hỗ trợ sớm nhất. Mã tra cứu: ${clientMessageId}`);
+        }else{
+          toastInfo( /*#__PURE__*/React.createElement(FormattedMessage, {
+            id: "common.error.500"
+          }));
+        }
     }
 
     store.dispatch({
