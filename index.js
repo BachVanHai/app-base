6ccc58f514d1f4a4d5c1760bd020d77194dd1c4b
DESKTOP-IVN453T\Admin <chudanhtuan2034@gmail.com>
2025-05-08 10:22:51 +0700

    [8/5] Điều chỉnh hiển thị message lỗi 400 & 500 4.0

--- index.js
+++ index.js
@@ -586,7 +586,11 @@ var setUpHttpClient = function setUpHttpClient(store, apiBaseUrl) {
     switch (e.response.status) {
       case 400:
       case 500: {
-        const clientMessageId = e.response.config.headers.clientmessageid || e.response.config.headers.clientMessageId || e.response.config.headers['client-message-id'];
+        let clientMessageId;
+        clientMessageId = e.response.config.headers.clientmessageid || 
+                         e.response.config.headers.clientMessageId || 
+                         e.response.config.headers['client-message-id'];
+        
         if(clientMessageId){
           toastInfo(e.response.data.message || `Có lỗi trong quá trình xử lý. Vui lòng cung cấp mã tra cứu của bạn cho IT BMK để được hỗ trợ sớm nhất. Mã tra cứu: ${clientMessageId}`);
         } else {
