c9f862922c19fbbb18f4826e0ed86efccb1f4da7
BachVanHai <haibv@bmktech.com.vn>
2025-05-23 12:32:24 +0700

    Revert "fix:"
    This reverts commit d7bad37e571215e6d6374ff248b51bbe0c6cce1a.

--- index.modern.js
+++ index.modern.js
@@ -10298,52 +10298,24 @@ const defaultMaskOptions = {
   allowNegative: false,
   allowLeadingZeroes: false
 };
-// const CurrencyInput = ({
-//   maskOptions,
-//   placeholder,
-//   ...inputProps
-// }) => {
-//   const {
-//     formatMessage
-//   } = useIntl();
-//   const currencyMask = createNumberMask({
-//     ...defaultMaskOptions,
-//     ...maskOptions
-//   });
-//   return /*#__PURE__*/React.createElement(MaskedInput, Object.assign({
-//     mask: currencyMask,
-//     placeholder: formatMessage({
-//       id: placeholder
-//     })
-//   }, inputProps));
-// };
 const CurrencyInput = ({
   maskOptions,
   placeholder,
-  messageId,
-  className,
   ...inputProps
 }) => {
-  const { formatMessage } = useIntl();
+  const {
+    formatMessage
+  } = useIntl();
   const currencyMask = createNumberMask({
     ...defaultMaskOptions,
     ...maskOptions
   });
-
-  return (
-    <FormGroup className={`form-label-group position-relative ${className || ''}`}>
-      {messageId && (
-        <Label>
-          <FormattedMessage id={messageId} />
-        </Label>
-      )}
-      <MaskedInput
-        mask={currencyMask}
-        placeholder={formatMessage({ id: placeholder })}
-        {...inputProps}
-      />
-    </FormGroup>
-  );
+  return /*#__PURE__*/React.createElement(MaskedInput, Object.assign({
+    mask: currencyMask,
+    placeholder: formatMessage({
+      id: placeholder
+    })
+  }, inputProps));
 };
 CurrencyInput.defaultProps = {
   inputMode: 'numeric',
