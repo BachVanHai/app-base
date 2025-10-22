fa2241e80f570b0912fff10a6201777ef7a8922a
DESKTOP-IVN453T\Admin <chudanhtuan2034@gmail.com>
2025-10-22 10:45:38 +0700

    user account - fallback avatar 2.0

--- index.modern.js
+++ index.modern.js
@@ -2465,7 +2465,7 @@ const NavbarUser = props => {
   }, user.fullName)), /*#__PURE__*/React.createElement("span", {
     "data-tour": "user"
   }, /*#__PURE__*/React.createElement("img", {
-    src: userSettings.avatar || '',
+    src: userSettingsDTO.avatar || 'https://sit2.bmktech.vn/resources/images/default-user-avatar.png',
     className: "round",
     height: "40",
     width: "40",
